07df18f188498904de15869a3ca3b7eb0399e85d
ursulakaczmarek <ursula.kaczmarek@gmail.com>
2024-11-01 10:11:50 -0600

    add code+output for 2024 day 1:points

---
+++ 2024/points/nyc_polling_places.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(RSocrata)
+library(sf)
+library(here)
+
+# read in airport lat/lon
+points <- read.socrata("https://data.cityofnewyork.us/resource/mifw-tguq.json?site_status=A", 
+            app_token = Sys.getenv("SOCRATA_KEY")) |> 
+  filter(!is.na(longitude)) |> 
+  st_as_sf(coords = c("longitude","latitude"), crs = "WGS84", remove = FALSE) |> 
+  select(city, site_name, nta, longitude, latitude) |> 
+  mutate(`site type` = if_else(grepl("PS|Elementary|School|HS|H.S.|IS |MS |Academy|College", site_name), 
+    "is a school", "is not a school"))
+
+# polygons of nyc borough boundaries with spatially joined polling place points
+nyc_poly <- st_read(here("2024/nyc_boundaries.geojson"))
+
+g <- ggplot() +
+  geom_sf(data = nyc_poly, fill = "#d8dbe2", color = "#355070") +
+  geom_sf(data = points, aes(color = `site type`, shape = `site type`), 
+          size = 0.8) +
+  scale_color_manual(values = c("#e07a5f", "#4b3f72")) +
+  labs(title = "NYC Polling Sites: 2024 General Election",
+       caption = "source:  data.cityofnewyork.us") +
+  theme_void() +
+  theme(plot.title = element_text(color = "#355070", size = 14, hjust = 0.25),
+        legend.title = element_blank(),
+        legend.margin = margin(5, 5, 5, 5, "pt"))
+
+
+outfile <- here("2024/images/nyc_polls.png")
+ggsave(outfile, bg = "#d8dbe2")
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 i lived in the country of georgia for a few years, so i will publish maps about that beautiful place
 
 i aim to create a minimum of ten maps and use the `rayshader` , `stars`, and `magick` packages in R for the first time
+
+### 2024
+
+i work for a civic tech nonprofit focused on voting, so i will publish maps relating to elections
+
+i aim to create a minimum of fifteen maps this year even though work keeps me busy during election season (the u.s. general election falls on day 5: 'a journey')
